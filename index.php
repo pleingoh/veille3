11619aff0569b641bcfcb56b1ee8ee7a772c38c1
aminata <aminatasoumahoro2@gmail.com>
2021-07-22 14:15:26 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Veille SIMPLON</title>
+</head>
+<body>
+<div class='container'>
+
+        <p>
+            <h1> VEILLE TECHNOLOGIQUE SIMPLON 3</h1>  
+            <h2>Thème : DBMS (Data base management système)</h2>
+        </p>
+        <p>
+            <h3>I.	C’est quoi un DBMS?</h3>
+            Un système de gestion de base de données en français (abr. SGBD) est un logiciel système servant à stocker, à manipuler ou gérer, et à partager des données dans une base de données, en garantissant la qualité, la pérennité et la confidentialité des informations, tout en cachant la complexité des opérations.
+        </p>
+        <p>
+            <h3>II.	Quelques définitions ?</h3>
+            Nous ne pouvons continuer sans définir certains termes
+            <h4> -	Système de gestion   </h4>
+            Un système de gestion (management) est un ensemble d'éléments corrélés ou interactifs permettant à un organisme d'établir une politique, des objectifs et d'atteindre ces objectifs.
+            <h4>-	Base de données </h4>
+            Ensemble d'informations structurées accessibles au moyen d'un logiciel.
+            <h4>-	Logiciel système</h4>
+            Le logiciel système est un ensemble de programmes informatiques et de bibliothèques logicielles qui fournit un environnement permettant de créer et d'exécuter des logiciels applicatifs.
+        </p>
+        <p> <h4>Un SGBD est un logiciel ou ensemble de programme permettant la gestion d’informations ou de données concernant une entité mis en relation de manière structurée.</h4> </p>
+        <p>
+            <h3>III.	Exemple de SGBD</h3>
+            Ils existent plusieurs SGBD certains avec de forte communauté et d’autres moins connus
+        </p>
+        <div> <img src="sgbd.png" alt=""></div>
+</div>
+</body>
+</html>
